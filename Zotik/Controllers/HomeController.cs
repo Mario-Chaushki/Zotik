ab6dc99ace7e082919d71de741decf9fcb38ccd7
Mario Chaushki <chaushki.mario@gmail.com>
2018-03-31 17:39:39 +0300

    returned products in home controller. added foreach pad in home/products

--- Zotik/Controllers/HomeController.cs
+++ Zotik/Controllers/HomeController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Zotik.Models;
 
 namespace Zotik.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly PadsContext context;
+
+        public HomeController(PadsContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             ViewBag.Title = "Начална страница";
             return View();
+        }
+
+        public IActionResult Products()
+        {
+            ViewBag.Title = "Продукти";
+
+            var pads = context.Pad.ToList(); ////Your model that you want to pass
+            return View(pads);
         }
     }
 }
