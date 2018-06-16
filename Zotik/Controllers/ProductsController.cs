c238761e1296c7788ba85640bb605b1cff0dcca5
Mario Chaushki <chaushki.mario@gmail.com>
2018-06-16 12:36:45 +0300

    managing products autorization

--- Zotik/Controllers/ProductsController.cs
+++ Zotik/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,7 @@ namespace Zotik.Controllers
         }
 
         // GET: Products
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Index()
         {
             ViewBag.Title = "Продукти";
@@ -26,6 +28,7 @@ namespace Zotik.Controllers
         }
 
         // GET: Products/Details/5
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,6 +47,7 @@ namespace Zotik.Controllers
         }
 
         // GET: Products/Create
+        [Authorize(Roles = "Administrator")]
         public IActionResult Create()
         {
             return View();
@@ -54,6 +58,7 @@ namespace Zotik.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Create([Bind("Id,Name,Description,Type,Material,ImageName")] Pad pad)
         {
             if (ModelState.IsValid)
@@ -66,6 +71,7 @@ namespace Zotik.Controllers
         }
 
         // GET: Products/Edit/5
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -86,6 +92,7 @@ namespace Zotik.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Type,Material,ImageName")] Pad pad)
         {
             if (id != pad.Id)
@@ -117,6 +124,7 @@ namespace Zotik.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -137,6 +145,7 @@ namespace Zotik.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var pad = await _context.Pad.SingleOrDefaultAsync(m => m.Id == id);
